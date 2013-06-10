84b86400a5938ac56155ce230fb8177dfc3acef2
JonathanIvan <jirp_salchi@hotmail.com>
2013-06-10 18:54:04 -0500

    Pricipal ventana de inicio del programa y coneccion a mysql con un datagrid

---
+++ ProyectoFinal/Principal.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ProyectoFinal
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.dtgdirectorio = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VER = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.EDITAR = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ELIMINAR = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ENVIAR = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgdirectorio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(425, 5);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(56, 27);
+            this.btnNuevo.TabIndex = 9;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(628, 7);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(63, 25);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnExportar);
+            this.panel1.Controls.Add(this.dtgdirectorio);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.btnNuevo);
+            this.panel1.Location = new System.Drawing.Point(-2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(739, 414);
+            this.panel1.TabIndex = 12;
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(530, 5);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(56, 27);
+            this.btnExportar.TabIndex = 13;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // dtgdirectorio
+            // 
+            this.dtgdirectorio.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgdirectorio.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dtgdirectorio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgdirectorio.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.AP,
+            this.AM,
+            this.Telefono,
+            this.VER,
+            this.EDITAR,
+            this.ELIMINAR,
+            this.ENVIAR});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgdirectorio.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dtgdirectorio.Location = new System.Drawing.Point(14, 38);
+            this.dtgdirectorio.Name = "dtgdirectorio";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgdirectorio.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dtgdirectorio.Size = new System.Drawing.Size(722, 262);
+            this.dtgdirectorio.TabIndex = 12;
+            this.dtgdirectorio.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgdirectorio_CellContentClick);
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // AP
+            // 
+            this.AP.HeaderText = "Apellido Paterno";
+            this.AP.Name = "AP";
+            // 
+            // AM
+            // 
+            this.AM.HeaderText = "Apellido Materno";
+            this.AM.Name = "AM";
+            // 
+            // Telefono
+            // 
+            this.Telefono.HeaderText = "Telefono";
+            this.Telefono.Name = "Telefono";
+            // 
+            // VER
+            // 
+            this.VER.HeaderText = "Ver";
+            this.VER.Name = "VER";
+            this.VER.Text = "Ver";
+            // 
+            // EDITAR
+            // 
+            this.EDITAR.HeaderText = "Editar";
+            this.EDITAR.Name = "EDITAR";
+            // 
+            // ELIMINAR
+            // 
+            this.ELIMINAR.HeaderText = "Eliminar";
+            this.ELIMINAR.Name = "ELIMINAR";
+            // 
+            // ENVIAR
+            // 
+            this.ENVIAR.HeaderText = "Enviar Email";
+            this.ENVIAR.Name = "ENVIAR";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 415);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Principal";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgdirectorio)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dtgdirectorio;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+        private System.Windows.Forms.DataGridViewButtonColumn VER;
+        private System.Windows.Forms.DataGridViewButtonColumn EDITAR;
+        private System.Windows.Forms.DataGridViewButtonColumn ELIMINAR;
+        private System.Windows.Forms.DataGridViewButtonColumn ENVIAR;
+    }
+}
+
---
+++ ProyectoFinal/Principal.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class Principal : Form
+    {
+
+        public Principal()
+        {
+            InitializeComponent();
+          
+            
+        }
+       
+           
+      MySqlConnection miconexion=new MySqlConnection("Server=127.0.0.1; Database=directorio; Uid=root; Password=208081433;");
+     
+      public void carga_Principal() {
+          MySqlCommand comando = new MySqlCommand("select *from contacto", miconexion);
+          miconexion.Open();
+          MySqlDataReader leer=comando.ExecuteReader();
+          dtgdirectorio.Rows.Clear();
+          
+          int renglon = 0;
+          
+          while (leer.Read()) {
+              
+              renglon = dtgdirectorio.Rows.Add();
+              dtgdirectorio.Rows[renglon].Cells["Nombre"].Value = leer.GetString(3);
+              dtgdirectorio.Rows[renglon].Cells["AP"].Value = leer.GetString(1);
+              dtgdirectorio.Rows[renglon].Cells["AM"].Value = leer.GetString(2);
+              dtgdirectorio.Rows[renglon].Cells["Telefono"].Value = leer.GetString(11);
+              
+              DataGridViewButtonColumn VER = new DataGridViewButtonColumn();
+              VER.Name = "ver_" + leer.GetString(0);
+              dtgdirectorio.Rows[renglon].Cells["VER"].Value = VER;
+
+              DataGridViewButtonColumn ELIMINAR = new DataGridViewButtonColumn();
+              ELIMINAR.Name = "eliminar_" + leer.GetString(0);
+              dtgdirectorio.Rows[renglon].Cells["ELIMINAR"].Value = ELIMINAR;
+
+              DataGridViewButtonColumn EDITAR = new DataGridViewButtonColumn();
+              EDITAR.Name = "editar_" + leer.GetString(0);
+              dtgdirectorio.Rows[renglon].Cells["EDITAR"].Value = EDITAR;
+
+              
+
+              DataGridViewButtonColumn ENVIAR = new DataGridViewButtonColumn();
+              ENVIAR.Name = "enviar_" + leer.GetString(0);
+              dtgdirectorio.Rows[renglon].Cells["ENVIAR"].Value = ENVIAR;
+
+              
+          }
+          miconexion.Close();
+      }
+
+
+    
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            carga_Principal();
+            
+            
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            
+            
+            VentanaNuevo ventana_nuevo = new VentanaNuevo(this);
+            
+            ventana_nuevo.Show();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            VentanaBuscar ventanabuscar = new VentanaBuscar();
+            ventanabuscar.Show();
+            //this.SetVisibleCore(false);
+            
+        }
+
+
+   
+        private void dtgdirectorio_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dtgdirectorio.Columns["VER"].Index)
+            {
+                DataGridViewButtonColumn boton = (DataGridViewButtonColumn) dtgdirectorio.Rows[e.RowIndex].Cells["VER"].Value;
+                String id = boton.Name.Replace("ver_","");
+                VentanaVer ver = new VentanaVer(id);
+                ver.Show();
+            }else if (e.ColumnIndex == dtgdirectorio.Columns["EDITAR"].Index)
+            {
+                DataGridViewButtonColumn boton = (DataGridViewButtonColumn)dtgdirectorio.Rows[e.RowIndex].Cells["EDITAR"].Value;
+                String id = boton.Name.Replace("editar_", "");
+                VentanaEditar editar = new VentanaEditar (this,id);
+                editar.Show();
+            }else if (e.ColumnIndex == dtgdirectorio.Columns["ELIMINAR"].Index)
+            {
+                DataGridViewButtonColumn boton = (DataGridViewButtonColumn)dtgdirectorio.Rows[e.RowIndex].Cells["ELIMINAR"].Value;
+                String id = boton.Name.Replace("eliminar_", "");
+                VenEliminar eliminar = new VenEliminar(this,id);
+                
+                //eliminar.Show();
+            }
+            else if (e.ColumnIndex == dtgdirectorio.Columns["ENVIAR"].Index)
+            {
+                DataGridViewButtonColumn boton = (DataGridViewButtonColumn)dtgdirectorio.Rows[e.RowIndex].Cells["ENVIAR"].Value;
+                String id = boton.Name.Replace("enviar_", "");
+                VentanaEnviar enviar = new VentanaEnviar(id);
+
+                enviar.Show();
+            }
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            
+            Microsoft.Office.Interop.Excel.Application Excel_Application;
+            Excel._Workbook HojaTrabajo;
+            Excel._Worksheet PestañaTrabajo;
+            //Excel.Range ColumnaRango;
+
+            Excel_Application = new Microsoft.Office.Interop.Excel.Application();
+
+            object misValue = System.Reflection.Missing.Value;
+            HojaTrabajo = Excel_Application.Workbooks.Add(misValue);
+            PestañaTrabajo = (Excel.Worksheet)HojaTrabajo.ActiveSheet;
+
+            //PestañaTrabajo.get_Range("A1", "D1").Font.Bold = true;
+            //PestañaTrabajo.get_Range("A1", "D1").Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.LightBlue);
+          
+            int i = 0;
+            int j = 0;
+
+            for (i = 1; i < dtgdirectorio.Columns.Count + 1; i++) {
+
+                PestañaTrabajo.Cells[1, i] = dtgdirectorio.Columns[i - 1].HeaderText;
+
+            }
+            for (i = 1; i < dtgdirectorio.RowCount - 1; i++) {
+
+                for (j = 1; j < dtgdirectorio.Columns.Count - 1; j++) {
+
+                    DataGridViewCell cell = dtgdirectorio[j, i];
+                    PestañaTrabajo.Cells[i + 2, j + 1]=cell.Value;
+
+                }
+            }
+
+            string Carpeta = @"C:\ArchivosExcel\";
+            if(!Directory.Exists(Carpeta)){
+                Directory.CreateDirectory(Carpeta);
+            }
+            string archivo = Carpeta + "Contacto.xls";
+            try
+            {
+                HojaTrabajo.SaveAs(archivo,Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue,misValue,misValue,Excel.XlSaveAsAccessMode.xlExclusive,misValue,misValue,misValue,misValue,misValue);
+                System.Diagnostics.Process.Start(Carpeta);
+            }
+            catch { }
+            Excel_Application.Quit();
+
+
+        }
+
+       
+    }
+}
