53427f08ed6a9d406f557ada7f3a238c7d3380b1
JonathanIvan <jirp_salchi@hotmail.com>
2013-06-10 19:05:30 -0500

    todo

---
+++ ProyectoFinal/Directorio.cs
@@ -0,0 +1,30 @@
+using System;
+namespace ProyectoFinal
+{
+	public class Directorio
+	{
+		public string id;
+        public string nombre;
+		public string ap;
+        public string am;
+        public string domicilio;
+        public string colonia;
+        public string cp;
+        public string municipio;
+        public string estado;
+        public string pais;
+        public string mapa;
+        public string telefono;
+        public string celular;
+        public string radio;
+        public string email;
+        public string observaciones;
+        public string saved_at;
+        public string modified_in;
+        
+		public Directorio ()
+		{
+		}
+	}
+}
+
---
+++ ProyectoFinal/VenEliminar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace ProyectoFinal
+{
+    class VenEliminar
+    {
+
+        private MySqlConnection miconexion = new MySqlConnection("Server=127.0.0.1; Database=directorio; Uid=root; Password=208081433;");
+        private String id;
+        private Principal padre;
+
+        public VenEliminar(Principal padre, string id)
+        {
+            this.padre = padre;
+            // TODO: Complete member initialization
+            this.id = id;
+
+            DialogResult resultado = MessageBox.Show("¿Realmende desea Eliminar Contacto", "AVISO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (resultado == DialogResult.No)
+            {
+                return;
+            }
+            this.carga_Contacto();
+        }
+        public void carga_Contacto()
+        {
+            
+            MySqlCommand comando = new MySqlCommand("Delete FROM contacto where id ='" + this.id + "'", miconexion);
+            
+            miconexion.Open();
+            comando.ExecuteNonQuery();
+            miconexion.Close();
+           MessageBox.Show("Contacto Borrado Correctamente");
+           this.padre.carga_Principal();
+    }
+  }
+}
