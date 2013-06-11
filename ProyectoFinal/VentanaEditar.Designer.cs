ad5a4026ec59805ab2ce9728432c41d1280a0a21
JonathanIvan <jirp_salchi@hotmail.com>
2013-06-10 19:00:19 -0500

    Ventana edita los contactos del datagrid y la base de datos

---
+++ ProyectoFinal/VentanaEditar.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace ProyectoFinal
+{
+    partial class VentanaEditar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtObservaciones = new System.Windows.Forms.RichTextBox();
+            this.btnedit = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtRadio = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblObserva = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtMunicipio = new System.Windows.Forms.TextBox();
+            this.txtCp = new System.Windows.Forms.TextBox();
+            this.txtColonia = new System.Windows.Forms.TextBox();
+            this.txtDomicilio = new System.Windows.Forms.TextBox();
+            this.txtAm = new System.Windows.Forms.TextBox();
+            this.txtAp = new System.Windows.Forms.TextBox();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblMunicipio = new System.Windows.Forms.Label();
+            this.lblCp = new System.Windows.Forms.Label();
+            this.lblColonia = new System.Windows.Forms.Label();
+            this.lblDomicilio = new System.Windows.Forms.Label();
+            this.lblAm = new System.Windows.Forms.Label();
+            this.lblAp = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblMapa = new System.Windows.Forms.Label();
+            this.txtMapa = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtMapa);
+            this.panel1.Controls.Add(this.lblMapa);
+            this.panel1.Controls.Add(this.txtObservaciones);
+            this.panel1.Controls.Add(this.btnedit);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtRadio);
+            this.panel1.Controls.Add(this.txtCelular);
+            this.panel1.Controls.Add(this.txtTelefono);
+            this.panel1.Controls.Add(this.lblObserva);
+            this.panel1.Controls.Add(this.lblEmail);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.lblTelefono);
+            this.panel1.Controls.Add(this.txtPais);
+            this.panel1.Controls.Add(this.txtEstado);
+            this.panel1.Controls.Add(this.txtMunicipio);
+            this.panel1.Controls.Add(this.txtCp);
+            this.panel1.Controls.Add(this.txtColonia);
+            this.panel1.Controls.Add(this.txtDomicilio);
+            this.panel1.Controls.Add(this.txtAm);
+            this.panel1.Controls.Add(this.txtAp);
+            this.panel1.Controls.Add(this.lblPais);
+            this.panel1.Controls.Add(this.lblEstado);
+            this.panel1.Controls.Add(this.lblMunicipio);
+            this.panel1.Controls.Add(this.lblCp);
+            this.panel1.Controls.Add(this.lblColonia);
+            this.panel1.Controls.Add(this.lblDomicilio);
+            this.panel1.Controls.Add(this.lblAm);
+            this.panel1.Controls.Add(this.lblAp);
+            this.panel1.Controls.Add(this.txtNom);
+            this.panel1.Controls.Add(this.lblNom);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(592, 307);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtObservaciones
+            // 
+            this.txtObservaciones.Location = new System.Drawing.Point(131, 230);
+            this.txtObservaciones.Name = "txtObservaciones";
+            this.txtObservaciones.Size = new System.Drawing.Size(206, 65);
+            this.txtObservaciones.TabIndex = 58;
+            this.txtObservaciones.Text = "";
+            // 
+            // btnedit
+            // 
+            this.btnedit.Location = new System.Drawing.Point(512, 228);
+            this.btnedit.Name = "btnedit";
+            this.btnedit.Size = new System.Drawing.Size(75, 23);
+            this.btnedit.TabIndex = 57;
+            this.btnedit.Text = "Editar";
+            this.btnedit.UseVisualStyleBackColor = true;
+            this.btnedit.Click += new System.EventHandler(this.btnedit_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(58, 189);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(131, 20);
+            this.txtEmail.TabIndex = 56;
+            // 
+            // txtRadio
+            // 
+            this.txtRadio.Location = new System.Drawing.Point(450, 147);
+            this.txtRadio.Name = "txtRadio";
+            this.txtRadio.Size = new System.Drawing.Size(131, 20);
+            this.txtRadio.TabIndex = 55;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(265, 147);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(131, 20);
+            this.txtCelular.TabIndex = 54;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(58, 146);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(131, 20);
+            this.txtTelefono.TabIndex = 53;
+            // 
+            // lblObserva
+            // 
+            this.lblObserva.AutoSize = true;
+            this.lblObserva.Location = new System.Drawing.Point(47, 233);
+            this.lblObserva.Name = "lblObserva";
+            this.lblObserva.Size = new System.Drawing.Size(78, 13);
+            this.lblObserva.TabIndex = 52;
+            this.lblObserva.Text = "Observaciones";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(26, 196);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 51;
+            this.lblEmail.Text = "Email";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(409, 153);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(35, 13);
+            this.label12.TabIndex = 50;
+            this.label12.Text = "Radio";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(197, 150);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 13);
+            this.label11.TabIndex = 49;
+            this.label11.Text = "Celular";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(9, 150);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefono.TabIndex = 48;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(426, 113);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(131, 20);
+            this.txtPais.TabIndex = 47;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(256, 113);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(131, 20);
+            this.txtEstado.TabIndex = 46;
+            // 
+            // txtMunicipio
+            // 
+            this.txtMunicipio.Location = new System.Drawing.Point(67, 113);
+            this.txtMunicipio.Name = "txtMunicipio";
+            this.txtMunicipio.Size = new System.Drawing.Size(131, 20);
+            this.txtMunicipio.TabIndex = 45;
+            // 
+            // txtCp
+            // 
+            this.txtCp.Location = new System.Drawing.Point(426, 76);
+            this.txtCp.Name = "txtCp";
+            this.txtCp.Size = new System.Drawing.Size(131, 20);
+            this.txtCp.TabIndex = 44;
+            // 
+            // txtColonia
+            // 
+            this.txtColonia.Location = new System.Drawing.Point(245, 76);
+            this.txtColonia.Name = "txtColonia";
+            this.txtColonia.Size = new System.Drawing.Size(131, 20);
+            this.txtColonia.TabIndex = 43;
+            // 
+            // txtDomicilio
+            // 
+            this.txtDomicilio.Location = new System.Drawing.Point(59, 76);
+            this.txtDomicilio.Name = "txtDomicilio";
+            this.txtDomicilio.Size = new System.Drawing.Size(131, 20);
+            this.txtDomicilio.TabIndex = 42;
+            // 
+            // txtAm
+            // 
+            this.txtAm.Location = new System.Drawing.Point(477, 36);
+            this.txtAm.Name = "txtAm";
+            this.txtAm.Size = new System.Drawing.Size(104, 20);
+            this.txtAm.TabIndex = 41;
+            // 
+            // txtAp
+            // 
+            this.txtAp.Location = new System.Drawing.Point(271, 36);
+            this.txtAp.Name = "txtAp";
+            this.txtAp.Size = new System.Drawing.Size(105, 20);
+            this.txtAp.TabIndex = 40;
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Location = new System.Drawing.Point(393, 116);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(27, 13);
+            this.lblPais.TabIndex = 39;
+            this.lblPais.Text = "Pais";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(210, 116);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(40, 13);
+            this.lblEstado.TabIndex = 38;
+            this.lblEstado.Text = "Estado";
+            // 
+            // lblMunicipio
+            // 
+            this.lblMunicipio.AutoSize = true;
+            this.lblMunicipio.Location = new System.Drawing.Point(9, 116);
+            this.lblMunicipio.Name = "lblMunicipio";
+            this.lblMunicipio.Size = new System.Drawing.Size(52, 13);
+            this.lblMunicipio.TabIndex = 37;
+            this.lblMunicipio.Text = "Municipio";
+            // 
+            // lblCp
+            // 
+            this.lblCp.AutoSize = true;
+            this.lblCp.Location = new System.Drawing.Point(393, 79);
+            this.lblCp.Name = "lblCp";
+            this.lblCp.Size = new System.Drawing.Size(27, 13);
+            this.lblCp.TabIndex = 36;
+            this.lblCp.Text = "C.P.";
+            // 
+            // lblColonia
+            // 
+            this.lblColonia.AutoSize = true;
+            this.lblColonia.Location = new System.Drawing.Point(197, 79);
+            this.lblColonia.Name = "lblColonia";
+            this.lblColonia.Size = new System.Drawing.Size(42, 13);
+            this.lblColonia.TabIndex = 35;
+            this.lblColonia.Text = "Colonia";
+            // 
+            // lblDomicilio
+            // 
+            this.lblDomicilio.AutoSize = true;
+            this.lblDomicilio.Location = new System.Drawing.Point(9, 79);
+            this.lblDomicilio.Name = "lblDomicilio";
+            this.lblDomicilio.Size = new System.Drawing.Size(49, 13);
+            this.lblDomicilio.TabIndex = 34;
+            this.lblDomicilio.Text = "Domicilio";
+            // 
+            // lblAm
+            // 
+            this.lblAm.AutoSize = true;
+            this.lblAm.Location = new System.Drawing.Point(385, 39);
+            this.lblAm.Name = "lblAm";
+            this.lblAm.Size = new System.Drawing.Size(86, 13);
+            this.lblAm.TabIndex = 33;
+            this.lblAm.Text = "Apellido Materno";
+            // 
+            // lblAp
+            // 
+            this.lblAp.AutoSize = true;
+            this.lblAp.Location = new System.Drawing.Point(181, 39);
+            this.lblAp.Name = "lblAp";
+            this.lblAp.Size = new System.Drawing.Size(84, 13);
+            this.lblAp.TabIndex = 32;
+            this.lblAp.Text = "Apellido Paterno";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(59, 36);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(111, 20);
+            this.txtNom.TabIndex = 31;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(9, 39);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(44, 13);
+            this.lblNom.TabIndex = 30;
+            this.lblNom.Text = "Nombre";
+            // 
+            // lblMapa
+            // 
+            this.lblMapa.AutoSize = true;
+            this.lblMapa.Location = new System.Drawing.Point(210, 192);
+            this.lblMapa.Name = "lblMapa";
+            this.lblMapa.Size = new System.Drawing.Size(34, 13);
+            this.lblMapa.TabIndex = 59;
+            this.lblMapa.Text = "Mapa";
+            // 
+            // txtMapa
+            // 
+            this.txtMapa.Location = new System.Drawing.Point(256, 189);
+            this.txtMapa.Name = "txtMapa";
+            this.txtMapa.Size = new System.Drawing.Size(131, 20);
+            this.txtMapa.TabIndex = 60;
+            // 
+            // VentanaEditar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 308);
+            this.Controls.Add(this.panel1);
+            this.Name = "VentanaEditar";
+            this.Text = "Editar";
+            this.Load += new System.EventHandler(this.VentanaEditar_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox txtObservaciones;
+        private System.Windows.Forms.Button btnedit;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtRadio;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblObserva;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtMunicipio;
+        private System.Windows.Forms.TextBox txtCp;
+        private System.Windows.Forms.TextBox txtColonia;
+        private System.Windows.Forms.TextBox txtDomicilio;
+        private System.Windows.Forms.TextBox txtAm;
+        private System.Windows.Forms.TextBox txtAp;
+        private System.Windows.Forms.Label lblPais;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblMunicipio;
+        private System.Windows.Forms.Label lblCp;
+        private System.Windows.Forms.Label lblColonia;
+        private System.Windows.Forms.Label lblDomicilio;
+        private System.Windows.Forms.Label lblAm;
+        private System.Windows.Forms.Label lblAp;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.TextBox txtMapa;
+        private System.Windows.Forms.Label lblMapa;
+    }
+}
---
+++ ProyectoFinal/VentanaEditar.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoFinal
+{
+    public partial class VentanaEditar : Form
+    {
+        private MySqlConnection miconexion = new MySqlConnection("Server=127.0.0.1; Database=directorio; Uid=root; Password=208081433;");
+        private String id;
+        private Principal padre;
+        
+        public VentanaEditar(Principal padre, string id)
+        {
+            this.padre = padre;
+            InitializeComponent();
+            // TODO: Complete member initialization
+            this.id = id;
+            this.Text = "ID: " + id;
+            carga_datos();
+        }
+
+        private void btnedit_Click(object sender, EventArgs e)
+        {
+            MySqlCommand comando = new MySqlCommand("update contacto set nombre=@NAME,ap=@APP,am=@AMP,domicilio=@DOMICILIOO,colonia=@COLONIAA,cp=@CPP,municipio=@MUNICIPIOO,estado=@ESTADOO,pais=@PAISS,mapa=@MAPAA,telefono=@TEL,celular=@CEL,radio=@RADIOO,email=@EMAILL,observaciones=@OBSER where id ='" + this.id + "'", miconexion);
+            
+            comando.Parameters.AddWithValue("NAME", txtNom.Text);
+            comando.Parameters.AddWithValue("APP", txtAp.Text);
+            comando.Parameters.AddWithValue("AMP", txtAm.Text);
+            comando.Parameters.AddWithValue("DOMICILIOO", txtDomicilio.Text);
+            comando.Parameters.AddWithValue("COLONIAA", txtColonia.Text);
+            comando.Parameters.AddWithValue("CPP", txtCp.Text);
+            comando.Parameters.AddWithValue("MUNICIPIOO", txtMunicipio.Text);
+            comando.Parameters.AddWithValue("ESTADOO", txtEstado.Text);
+            comando.Parameters.AddWithValue("PAISS", txtPais.Text);
+            comando.Parameters.AddWithValue("MAPAA", txtMapa.Text);
+            comando.Parameters.AddWithValue("TEL", txtTelefono.Text);
+            comando.Parameters.AddWithValue("CEL", txtCelular.Text);
+            comando.Parameters.AddWithValue("RADIOO", txtRadio.Text);
+            comando.Parameters.AddWithValue("EMAILL", txtEmail.Text);
+            comando.Parameters.AddWithValue("OBSER", txtObservaciones.Text);
+            miconexion.Open();
+           comando.ExecuteNonQuery();
+            miconexion.Close();
+            MessageBox.Show("Editado");
+            this.padre.carga_Principal();
+        }
+
+        private void VentanaEditar_Load(object sender, EventArgs e)
+        {
+            
+        }
+        void carga_datos() {
+            string sql = "SELECT * FROM contacto WHERE id = '" + this.id + "'";
+            MySqlCommand comando = new MySqlCommand(sql, miconexion);
+            miconexion.Open();
+            MySqlDataReader leer = comando.ExecuteReader();
+            leer.Read();
+            if (!leer.IsDBNull(3))
+            {
+                this.txtNom.Text = leer.GetString(3);
+            }
+            else {
+                this.txtNom.Text = " ";
+            } if (!leer.IsDBNull(1))
+            {
+                this.txtAp.Text = leer.GetString(1);
+            }
+            else {
+                this.txtAp.Text = " ";
+            } if (!leer.IsDBNull(2))
+            {
+                this.txtAm.Text = leer.GetString(2);
+            }
+            else {
+                this.txtAm.Text = " ";
+            } if (!leer.IsDBNull(12))
+            {
+                this.txtCelular.Text = leer.GetString(12);
+            }
+            else {
+                this.txtCelular.Text = " ";
+            } if (!leer.IsDBNull(6))
+            {
+                this.txtCp.Text = leer.GetString(6);
+            }
+            else {
+                this.txtCp.Text = " ";
+            } if (!leer.IsDBNull(5))
+            {
+                this.txtColonia.Text = leer.GetString(5);
+            }
+            else {
+                this.txtColonia.Text = " ";
+            } if (!leer.IsDBNull(4))
+            {
+                this.txtDomicilio.Text = leer.GetString(4);
+            }
+            else {
+                this.txtDomicilio.Text = " ";
+            } if (!leer.IsDBNull(8))
+            {
+                this.txtEstado.Text = leer.GetString(8);
+            }
+            else {
+                this.txtEstado.Text = " ";
+            } if (!leer.IsDBNull(7))
+            {
+                this.txtMunicipio.Text = leer.GetString(7);
+            }
+            else {
+                this.txtMunicipio.Text = " ";
+            } if (!leer.IsDBNull(9))
+            {
+                this.txtPais.Text = leer.GetString(9);
+            }
+            else {
+                this.txtPais.Text = " ";
+            } if (!leer.IsDBNull(10))
+            {
+                this.txtMapa.Text = leer.GetString(10);
+            }
+            else {
+                this.txtMapa.Text = " ";
+            } if (!leer.IsDBNull(15))
+            {
+                this.txtObservaciones.Text = leer.GetString(15);
+            }
+            else {
+                this.txtObservaciones.Text = " ";
+            } if (!leer.IsDBNull(13))
+            {
+                this.txtRadio.Text = leer.GetString(13);
+            }
+            else {
+                this.txtRadio.Text = " ";
+            } if (!leer.IsDBNull(14))
+            {
+                this.txtEmail.Text = leer.GetString(14);
+            }
+            else {
+                this.txtEmail.Text = " ";
+            } if (!leer.IsDBNull(11))
+            {
+                this.txtTelefono.Text = leer.GetString(11);
+            }
+            else {
+                this.txtTelefono.Text = " ";
+            }
+            miconexion.Close();
+        }
+    }
+}
